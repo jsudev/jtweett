7bf0b29f252545d99f13a8054dde18cd974628dd
jsudev <jsu.genius@gmail.com>
2015-10-22 11:02:08 -0400

    Update searchTweets.php

--- searchTweets.php
+++ searchTweets.php
@@ -7,8 +7,8 @@ function getJsonTweets($query,$num_tweets){
  
         /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
         $settings = array(
-            'oauth_access_token' => "",
-            'oauth_access_token_secret' => "",
+            'oauth_access_token' => "2282799211-CRzVkglvx36PXwQ0EGC1utwwFs678Osxotn2CEl",
+            'oauth_access_token_secret' => "wKXcaIHTMMxL6gOXRd2NTvP6rOxIcUg64aWRUIQEesKc7",
             'consumer_key' => "NNNV5Fj9T0lo134eLHkTuNuid",
             'consumer_secret' => "8shGHkaVLlyd6DVf4XJVqxoFFNnMC32M1z2VdLD42ODCrLMcb8"
         );
